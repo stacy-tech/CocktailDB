0a949cbab90799f6d43c71121de4174d2b244434
stacy <stacydn23@gmail.com>
2020-12-01 22:50:57 -0500

    api class structure

--- lib/api.rb
+++ lib/api.rb
@@ -1,3 +1,14 @@
+require_relative "../config/environment"
 class Cocktaildb::API
-    
+    def initialize
+        @url = https://www.thecocktaildb.com/api/json/v1/1/random.php
+    end
+
+    def order_cocktail
+        URI(@url)
+        # uri = URI.parse(@url)
+        binding.pry
+    end
 end 
+
+Cocktaildb::API.new.order_cocktail
